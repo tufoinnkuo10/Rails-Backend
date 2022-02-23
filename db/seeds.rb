9c7210471945711e1abda8cb12e4c3d78dcd73e4
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2022-02-24 00:25:56 +0100

    create greeting api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Message.create!(description: 'Hello')
+Message.create!(description: 'Hola')
+Message.create!(description: 'Welcome')
+Message.create!(description: 'Salute')
+Message.create!(description: 'Bonjour')
